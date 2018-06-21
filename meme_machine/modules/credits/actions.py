5554a31d47878a8072ad9156b14d1534f45007ff
Joel Tio <joeltiojinhon@gmail.com>
2018-06-21 19:52:29 +0800

    Move limit_command_arg to helpers

--- meme_machine/modules/credits/actions.py
+++ meme_machine/modules/credits/actions.py
@@ -1,6 +1,9 @@
-from modules.base.actions import limit_command_arg
+from modules.base.helpers import limit_command_arg
 
 
 @limit_command_arg(2)
 async def donate(client, message, *args):
     raise NotImplementedError()
+    receiver, amount = args
+    await client.send_message(message.channel,
+                              f"Donated {amount}PP to {receiver}")
