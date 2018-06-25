9a521322eaf01b954cdf84433d6873e6bf8d6bce
Joel Tio <joeltiojinhon@gmail.com>
2018-06-25 22:59:18 +0800

    Add config table

---
+++ meme_machine/alembic/versions/a12885ad2333_create_config_table.py
@@ -0,0 +1,28 @@
+"""create config table
+
+Revision ID: a12885ad2333
+Revises: 9a224d04200a
+Create Date: 2018-06-25 22:57:40.375172
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a12885ad2333'
+down_revision = '9a224d04200a'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        "config",
+        sa.Column("name", sa.String, primary_key=True),
+        sa.Column("value", sa.String, nullable=False),
+    )
+
+
+def downgrade():
+    op.drop_table("config")
