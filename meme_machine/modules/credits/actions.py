00e2955154101634f5604b5952be3f8f9d99bf95
Joel Tio <joeltiojinhon@gmail.com>
2018-06-27 21:51:35 +0800

    Update imports to use module imports

--- meme_machine/modules/credits/actions.py
+++ meme_machine/modules/credits/actions.py
@@ -1,27 +1,27 @@
-from datetime import datetime, timedelta
-from database import create_session
-from modules.base.helpers import limit_command_arg, validate_num_of_mentions
 import random
+from datetime import datetime, timedelta
 
-from modules.base.models import get_or_create_user
-from modules.credits.models import (get_or_create_credit,
-                                    get_or_create_credit_action,
-                                    get_or_create_daily_range)
-from modules.credits.settings import *
-from modules.credits.helpers import validate_credit_arg, validate_credit_range
+import database as main_db
+
+import modules.base.helpers as base_helpers
+import modules.base.models as base_models
+
+import modules.credit.models as credit_models
+import modules.credits.settings as credit_settings
+import modules.credit.helpers as credit_helpers
 
 
-@limit_command_arg(2)
+@base_helpers.limit_command_arg(2)
 async def donate(client, message, receiver_tag, str_amount):
     # Validate incoming arguments
-    error = (validate_credit_arg(str_amount) or
-             validate_num_of_mentions(message.mentions, 1))
+    error = (credit_helpers.validate_credit_arg(str_amount) or
+             base_helpers.validate_num_of_mentions(message.mentions, 1))
 
     if error is None:
         # Check for self donation
         if message.author.mention == receiver_tag:
-            await client.send_message(message.channel,
-                                      DONATE_ERROR_SELF_DONATE)
+            await client.send_message(
+                message.channel, credit_settings.DONATE_ERROR_SELF_DONATE)
             return
     else:
         await client.send_message(message.channel, error)
@@ -33,20 +33,21 @@ async def donate(client, message, receiver_tag, str_amount):
     receiver_discord = message.mentions[0]
 
     # Start a session
-    session = create_session()
+    session = main_db.create_session()
 
     # 1. Create both users if they do not already exist
-    sender_user = get_or_create_user(session, sender_discord)
-    receiver_user = get_or_create_user(session, receiver_discord)
+    sender_user = base_models.get_or_create_user(session, sender_discord)
+    receiver_user = base_models.get_or_create_user(session, receiver_discord)
 
     # 2. Create their credit records if they do not already exist
-    sender_credit = get_or_create_credit(session, sender_user.id)
-    receiver_credit = get_or_create_credit(session, receiver_user.id)
+    sender_credit = credit_models.get_or_create_credit(session, sender_user.id)
+    receiver_credit = credit_models.get_or_create_credit(
+        session, receiver_user.id)
 
     # 3. Check if there is enough credits in the sending user
     if sender_credit.credits < amount:
         await client.send_message(
-            message.channel, DONATE_ERROR_INSUFFICIENT_CREDITS)
+            message.channel, credit_settings.DONATE_ERROR_INSUFFICIENT_CREDITS)
 
         session.commit()
         session.close()
@@ -60,15 +61,15 @@ async def donate(client, message, receiver_tag, str_amount):
     session.commit()
     session.close()
 
-    success_message = DONATE_SUCCESS.format(
+    success_message = credit_settings.DONATE_SUCCESS.format(
         amount=amount, receiver=receiver_tag)
     await client.send_message(message.channel, success_message)
 
 
-@limit_command_arg(2)
+@base_helpers.limit_command_arg(2)
 async def admin_add(client, message, receiver_tag, str_amount):
-    error = (validate_credit_arg(str_amount) or
-             validate_num_of_mentions(message.mentions, 1))
+    error = (credit_helpers.validate_credit_arg(str_amount) or
+             base_helpers.validate_num_of_mentions(message.mentions, 1))
 
     if error is not None:
         await client.send_message(message.channel, error)
@@ -78,10 +79,11 @@ async def admin_add(client, message, receiver_tag, str_amount):
     receiver_discord = message.mentions[0]
 
     # Start a session
-    session = create_session()
+    session = main_db.create_session()
 
-    receiver_user = get_or_create_user(session, receiver_discord)
-    receiver_credit = get_or_create_credit(session, receiver_user.id)
+    receiver_user = credit_models.get_or_create_user(session, receiver_discord)
+    receiver_credit = credit_models.get_or_create_credit(
+        session, receiver_user.id)
 
     # Increase the credit amount
     receiver_credit.credits += amount
@@ -89,16 +91,16 @@ async def admin_add(client, message, receiver_tag, str_amount):
     session.commit()
     session.close()
 
-    success_message = ADMIN_ADD_SUCCESS.format(
+    success_message = credit_settings.ADMIN_ADD_SUCCESS.format(
         amount=str_amount, receiver=receiver_tag)
 
     await client.send_message(message.channel, success_message)
 
 
-@limit_command_arg(2)
+@base_helpers.limit_command_arg(2)
 async def admin_remove(client, message, receiver_tag, str_amount):
-    error = (validate_credit_arg(str_amount) or
-             validate_num_of_mentions(message.mentions, 1))
+    error = (credit_helpers.validate_credit_arg(str_amount) or
+             base_helpers.validate_num_of_mentions(message.mentions, 1))
 
     if error is not None:
         await client.send_message(message.channel, error)
@@ -108,10 +110,11 @@ async def admin_remove(client, message, receiver_tag, str_amount):
     receiver_discord = message.mentions[0]
 
     # Start a session
-    session = create_session()
+    session = main_db.create_session()
 
-    receiver_user = get_or_create_user(session, receiver_discord)
-    receiver_credit = get_or_create_credit(session, receiver_user.id)
+    receiver_user = credit_models.get_or_create_user(session, receiver_discord)
+    receiver_credit = credit_models.get_or_create_credit(
+        session, receiver_user.id)
 
     # Find out how much should be deducted
     deduction = min(receiver_credit.credits, amount)
@@ -122,23 +125,24 @@ async def admin_remove(client, message, receiver_tag, str_amount):
     session.commit()
     session.close()
 
-    success_message = ADMIN_REMOVE_SUCCESS.format(
+    success_message = credit_settings.ADMIN_REMOVE_SUCCESS.format(
         amount=deduction, receiver=receiver_tag)
 
     await client.send_message(message.channel, success_message)
 
 
-@limit_command_arg(0)
+@base_helpers.limit_command_arg(0)
 async def daily(client, message):
     # Start a session
-    session = create_session()
+    session = main_db.create_session()
 
     # Get or create a user
     sender_discord = message.author
-    sender_user = get_or_create_user(session, sender_discord)
+    sender_user = base_models.get_or_create_user(session, sender_discord)
 
     # Get or create credit action
-    sender_credit_action = get_or_create_credit_action(session, sender_user.id)
+    sender_credit_action = credit_models.get_or_create_credit_action(
+        session, sender_user.id)
 
     # Check if the user is elgible for daily, i.e. current time after
     # next_daily
@@ -149,34 +153,36 @@ async def daily(client, message):
         # Not time yet
         if time_left_seconds < 60:
             # Less than a minute
-            result_message = DAILY_SECONDS_LEFT.format(
+            result_message = credit_settings.DAILY_SECONDS_LEFT.format(
                 seconds=int(time_left_seconds))
         elif time_left_seconds < 360:
             # Less than an hour
-            result_message = DAILY_MINUTES_LEFT.format(
+            result_message = credit_settings.DAILY_MINUTES_LEFT.format(
                 minutes=int(time_left_seconds//60))
         else:
             # Less than a day
             hours = int(time_left_seconds//3600)
             minutes = int((time_left_seconds - hours*3600)//60)
-            result_message = DAILY_HOURS_AND_MINUTES_LEFT.format(
-                hours=hours, minutes=minutes)
+            result_message = credit_settings.DAILY_HOURS_AND_MINUTES_LEFT \
+                .format(hours=hours, minutes=minutes)
     else:
         # Get daily max and min
-        daily_min_config, daily_max_config = get_or_create_daily_range(session)
+        daily_min_config, daily_max_config = \
+            credit_models.get_or_create_daily_range(session)
         daily_min = int(daily_min_config)
         daily_max = int(daily_max_config)
 
         # Give the daily
         amount = random.randint(daily_min, daily_max)
 
-        sender_credit = get_or_create_credit(session, sender_user.id)
+        sender_credit = credit_models.get_or_create_credit(
+            session, sender_user.id)
         sender_credit.credits += amount
 
         # Update next_daily
         sender_credit_action.next_daily = datetime.now() + timedelta(days=1)
 
-        result_message = DAILY_SUCCESS.format(amount=amount)
+        result_message = credit_settings.DAILY_SUCCESS.format(amount=amount)
 
     session.commit()
     session.close()
@@ -184,27 +190,29 @@ async def daily(client, message):
     await client.send_message(message.channel, result_message)
 
 
-@limit_command_arg(2)
+@base_helpers.limit_command_arg(2)
 async def admin_daily_amt(client, message, str_new_min, str_new_max):
     # Validate arguments
-    error = (validate_credit_arg(str_new_min) or
-             validate_credit_arg(str_new_max) or
-             validate_credit_range(int(str_new_min), int(str_new_max)))
+    error = (credit_helpers.validate_credit_arg(str_new_min) or
+             credit_helpers.validate_credit_arg(str_new_max) or
+             credit_helpers.validate_credit_range(
+                 int(str_new_min), int(str_new_max)))
 
     if error is not None:
         await client.send_message(message.channel, error)
         return
 
-    session = create_session()
+    session = main_db.create_session()
 
-    daily_min_config, daily_max_config = get_or_create_daily_range(session)
+    daily_min_config, daily_max_config = \
+        credit_models.get_or_create_daily_range(session)
     daily_min_config.value = str_new_min
     daily_max_config.value = str_new_max
 
     session.commit()
     session.close()
 
-    success_message = ADMIN_DAILY_AMT_SUCCESS.format(
+    success_message = credit_settings.ADMIN_DAILY_AMT_SUCCESS.format(
         start=str_new_min, end=str_new_max)
 
     await client.send_message(message.channel, success_message)
