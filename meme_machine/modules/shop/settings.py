17ffa43de93762dad93c964d137a733519003bfe
Joel Tio <joeltiojinhon@gmail.com>
2018-06-30 16:47:00 +0800

    Update shop display

--- meme_machine/modules/shop/settings.py
+++ meme_machine/modules/shop/settings.py
@@ -18,7 +18,7 @@
 
 Required values to be formatted:
 SHOP_DISPLAY_EACH_ITEM - {name}, {code_name}, {cost}, {stock}
-SHOP_DISPLAY_EACH_CATEGORY - {name}, {code_name}
+SHOP_DISPLAY_EACH_CATEGRY - {name}, {code_name}
 
 For example:
 SHOP_DISPLAY_EACH_ITEM = "{name} [id: {code_name}] - {cost}PP"
@@ -27,10 +27,10 @@ SHOP_DISPLAY_EACH_CATEGORY = "{name} [id: {code_name}]"
 If there are no items (or all items are not in stock), only the
 SHOP_DISPLAY_NO_ITEMS will displayed.
 """
-SHOP_DISPLAY_HEADER = "Welcome to the PP Shop!\n"
+SHOP_DISPLAY_HEADER = "```Welcome to the PP Shop!\n"
 SHOP_DISPLAY_EACH_CATEGORY = "{name} (id: {code_name}):"
-SHOP_DISPLAY_EACH_ITEM = "{name} [id: {code_name}] - {cost}PP"
+SHOP_DISPLAY_EACH_ITEM = "{stock:<3}x{name} [id: {code_name}] - {cost}PP"
 SHOP_DISPLAY_CATEGORY_SEP = "\n"
-SHOP_DISPLAY_FOOTER = ""
+SHOP_DISPLAY_FOOTER = "```"
 SHOP_DISPLAY_NO_ITEMS = ("Looks like there are no items right now. Check back "
                          "again later!")
