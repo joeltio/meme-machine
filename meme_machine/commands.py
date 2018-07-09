3c7100fa675fa832a53053b933c29f1ae04ab1a9
Joel Tio <joeltiojinhon@gmail.com>
2018-07-09 17:09:36 +0800

    Add raffle module

--- meme_machine/commands.py
+++ meme_machine/commands.py
@@ -1,8 +1,10 @@
 from modules.credits.commands import COMMANDS as credits_commands
 from modules.shop.commands import COMMANDS as shop_commands
+from modules.raffle.commands import COMMANDS as raffle_commands
 
 # Define all the commands and their mapped functions
 COMMANDS = {
     **credits_commands,
     **shop_commands,
+    **raffle_commands,
 }
