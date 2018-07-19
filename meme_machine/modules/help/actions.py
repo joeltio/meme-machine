11b7786284df7f5f780504038de591c55ba90b1a
Joel Tio <joeltiojinhon@gmail.com>
2018-07-19 14:28:56 +0800

    Add help command with varying arguments

--- meme_machine/modules/help/actions.py
+++ meme_machine/modules/help/actions.py
@@ -1,13 +1,27 @@
 import modules.help.settings as help_settings
 
 
-async def help(client, message, command):
-    # Remove the ! infront of the command if any
-    bare_command = command.lstrip("!")
+async def help(client, message, *args):
+    if len(args) > 1:
+        error_message = help_settings.HELP_ERROR_WRONG_NUM_ARGS.format(
+            given=len(args))
+        await client.send_message(message.channel, error_message)
+        return
+    elif len(args) == 1:
+        command = args[0]
+        # Remove the ! infront of the command if any
+        bare_command = command.lstrip("!")
 
-    if bare_command in help_settings.COMMAND_HELP:
-        await client.send_message(
-            message.channel, help_settings.COMMAND_HELP[bare_command])
+        if bare_command in help_settings.COMMAND_HELP:
+            await client.send_message(
+                message.channel, help_settings.COMMAND_HELP[bare_command])
+        else:
+            await client.send_message(
+                message.channel, help_settings.HELP_ERROR_COMMAND_NOT_FOUND)
     else:
-        await client.send_message(
-            message.channel, help_settings.HELP_ERROR_COMMAND_NOT_FOUND)
+        # Show list of commands
+        display_message = help_settings.HELP_DISPLAY_TITLE + "\n"
+
+        display_message += "\n".join(help_settings.COMMAND_HELP.keys())
+
+        await client.send_message(message.channel, display_message)
--- meme_machine/modules/help/settings.py
+++ meme_machine/modules/help/settings.py
@@ -1,4 +1,7 @@
 HELP_ERROR_COMMAND_NOT_FOUND = "There is no help for that command"
+HELP_ERROR_WRONG_NUM_ARGS = ("Error: Incorrect number of arguments. "
+                             "Expected <2. Given {given}.")
+HELP_DISPLAY_TITLE = "List of commands:"
 
 COMMAND_HELP = {
     "help": "You are using it right now. So I guess you got it figured out.",
