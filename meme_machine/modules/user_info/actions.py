5461dbe49f3f1835776a270d026478718e3e3f2c
Joel Tio <joeltiojinhon@gmail.com>
2018-07-19 13:54:04 +0800

    Refactor profile and add admin-see command

--- meme_machine/modules/user_info/actions.py
+++ meme_machine/modules/user_info/actions.py
@@ -14,28 +14,25 @@ import modules.user_info.models as user_info_models
 import modules.user_info.settings as user_info_settings
 
 
-@base_helpers.limit_command_arg(0)
-async def profile(client, message):
+async def _profile(client, message, discord_user):
     session = maindb.create_session()
 
-    # User
-    sender_discord = message.author
-    sender_user = base_models.get_or_create_user(session, sender_discord)
+    db_user = base_models.get_or_create_user(session, discord_user)
 
     embed = discord.Embed(
-        title=sender_discord.name,
+        title=discord_user.name,
         color=user_info_settings.PROFILE_DISPLAY_COLOR)
     embed.set_author(name=user_info_settings.PROFILE_DISPLAY_TITLE,
                      icon_url=base_settings.EMBED_AUTHOR_ICON)
     embed.set_thumbnail(
-        url=sender_discord.avatar_url or sender_discord.default_avatar_url)
+        url=discord_user.avatar_url or discord_user.default_avatar_url)
 
     # Credits
-    sender_credit = credit_models.get_or_create_credit(session, sender_user.id)
+    user_credit = credit_models.get_or_create_credit(session, db_user.id)
 
     # Add the credits field to the embed
     embed_credit_value = user_info_settings.PROFILE_DISPLAY_CREDIT_VALUE \
-        .format(credits=sender_credit.credits)
+        .format(credits=user_credit.credits)
     embed.add_field(
         name=user_info_settings.PROFILE_DISPLAY_CREDIT_NAME,
         value=embed_credit_value, inline=False)
@@ -44,9 +41,12 @@ async def profile(client, message):
     current_raffle = raffle_models.get_raffle(session)
     if current_raffle is not None:
         sender_raffle_slots = raffle_models.get_raffle_slot(
-            session, sender_user.id, current_raffle.id)
+            session, db_user.id, current_raffle.id)
 
-        sender_num_slots = sender_raffle_slots.slots or 0
+        if sender_raffle_slots is None:
+            sender_num_slots = 0
+        else:
+            sender_num_slots = sender_raffle_slots.slots
 
         embed_raffle_value = user_info_settings.PROFILE_DISPLAY_RAFFLE_VALUE \
             .format(user_slots=sender_num_slots,
@@ -61,7 +61,7 @@ async def profile(client, message):
         value=embed_raffle_value, inline=False)
 
     # Steam Profile Url
-    steam_url = user_info_models.get_steam_profile(session, sender_user.id) \
+    steam_url = user_info_models.get_steam_profile(session, db_user.id) \
         or user_info_settings.PROFILE_DISPLAY_NO_STEAM_URL
 
     embed_steam_url_value = \
@@ -74,6 +74,12 @@ async def profile(client, message):
     session.close()
 
     await client.send_message(message.channel, embed=embed)
+
+
+@base_helpers.limit_command_arg(0)
+async def profile(client, message):
+    sender_discord = message.author
+    await _profile(client, message, sender_discord)
 
 
 @base_helpers.limit_command_arg(1)
@@ -100,3 +106,16 @@ async def set_steam_profile(client, message, steam_profile_url):
     success_message = user_info_settings.SET_STEAM_PROFILE_SUCCESS.format(
         new_profile_url=steam_profile_url)
     await client.send_message(message.channel, success_message)
+
+
+@base_helpers.limit_command_arg(1)
+async def admin_see(client, message, user):
+    # Validate argument
+    error = base_helpers.validate_num_of_mentions(message.mentions, 1)
+
+    if error is not None:
+        await client.send_message(message.channel, error)
+        return
+
+    target_discord = message.mentions[0]
+    await _profile(client, message, target_discord)
--- meme_machine/modules/user_info/commands.py
+++ meme_machine/modules/user_info/commands.py
@@ -1,6 +1,7 @@
-from modules.user_info.actions import profile, set_steam_profile
+from modules.user_info.actions import profile, set_steam_profile, admin_see
 
 COMMANDS = {
     "profile": profile,
+    "admin-see": admin_see,
     "set-steam-profile": set_steam_profile,
 }
